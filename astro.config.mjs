b2dafaea55289dcddc58e1a5df8740ff481b3c45
Med-SD <saadimohammed98@gmail.com>
2026-01-21 09:15:31 +0100

    All url has been updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Med-SD.github.io',
+  site: 'https://med-sd.github.io',
   base: '/my-art-gallery',
 });
